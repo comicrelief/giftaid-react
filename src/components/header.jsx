ab7ae65a49380ab0c18bc20f565626e84a2c841d
Heleen3258 <h.mol@comicrelief.com>
2018-04-18 10:48:32 +0100

    2: added header logos

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,7 @@
 /* eslint-env browser */
 import React, { Component } from 'react';
+import logo from '../images/CR_logo.jpg';
+import campaignLogos from '../images/campaign_logos.png';
 // import propTypes from 'prop-types';
 
 
@@ -7,9 +9,9 @@ class Header extends Component {
   render() {
     return (
       <div>
-        <a href="https://www.comicrelief.com"><img src="images/CR_logo.jpg" alt="Go to Comic Relief" /></a>
+        <a href="https://www.comicrelief.com"><img src={logo} alt="Go to Comic Relief" /></a>
+        <img src={campaignLogos} alt="Red Nose Day and Sport Relief campaign logos" />
       </div>
-
     );
   }
 }
