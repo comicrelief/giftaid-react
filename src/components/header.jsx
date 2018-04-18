c9e9b3f313f92dc077b773b56e3e6a61bdf5ef89
Heleen3258 <h.mol@comicrelief.com>
2018-04-18 16:08:52 +0100

    2: header styling

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,6 @@
 /* eslint-env browser */
 import React, { Component } from 'react';
-import logo from '../images/CR_logo.jpg';
+import logo from '../images/CR_logo.png';
 import campaignLogos from '../images/campaign_logos.png';
 // import propTypes from 'prop-types';
 
@@ -9,8 +9,16 @@ class Header extends Component {
   render() {
     return (
       <div>
-        <a href="https://www.comicrelief.com"><img src={logo} alt="Go to Comic Relief" /></a>
-        <img src={campaignLogos} alt="Red Nose Day and Sport Relief campaign logos" />
+        <header role="banner">
+          <div className="header__inner-wrapper">
+            <a href="https://www.comicrelief.com" className="logo cr-logo" title="Comic Relief">
+              <img src={logo} alt="Go to Comic Relief" />
+            </a>
+            <div className="logo campaign-logo" >
+              <img src={campaignLogos} alt="Red Nose Day and Sport Relief campaign logos" />
+            </div>
+          </div>
+        </header>
       </div>
     );
   }
