5ce1a0b07063a18065253dc2f5b0e40c7842b9c0
Denise Lashlley <deniselashlley@gmail.com>
2018-07-04 15:38:39 +0100

    fix eslint issue

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './App.scss';
 import App from './components/App/App';
+
+import './App.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
