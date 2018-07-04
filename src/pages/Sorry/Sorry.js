1344848ff18ccafc01259b2f5af47b89fa91bb2b
Denise Lashlley <deniselashlley@gmail.com>
2018-07-04 16:08:54 +0100

    add sorry page

---
+++ src/pages/Sorry/Sorry.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Sorry extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Sorry</h1>
+      </div>
+    );
+  }
+}
+
