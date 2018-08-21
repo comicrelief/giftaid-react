51d94098b6d157626a20d9e6dcdcd66e2747e4eb
Andy E Phipps <a.phipps@comicrelief.com>
2018-08-21 17:31:58 +0100

    Break out giftaid component to place after postcode lookup

--- src/pages/Update/Update.js
+++ src/pages/Update/Update.js
@@ -1,6 +1,358 @@
 import React, { Component } from 'react';
+import propTypes from 'prop-types';
+import axios from 'axios';
+import InputField from '@comicrelief/storybook/src/components/InputField/InputField';
+import JustInTime from '@comicrelief/storybook/src/components/JustInTime/JustInTime';
+import PostcodeLookup from '@comicrelief/storybook/src/components/PostcodeLookup/PostcodeLookup';
+import defaultInputFieldsData from './defaultUpdateFields.json';
 
+const ENDPOINT_URL = process.env.REACT_APP_ENDPOINT_URL;
+let scrollTimeout;
+/**
+ * GiftAidForm class
+ * Returns elements on this form with default properties.
+ * Input field properties can be overridden by passing in a inputFieldOverrides object with the
+ * same structure as the defaultInputFieldProps object, e.g.:
+ * inputFieldOverrides = {
+ *   firstName: {
+ *     label: 'some text',
+ *   },
+ * }
+ */
 class Update extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputFieldProps: [],
+      firstUpdate: false,
+      formValidity: false,
+      showErrorMessages: false,
+      formDataError: null,
+      formDataSuccess: null,
+      validation: {
+        confirm: {
+          valid: false,
+          value: undefined,
+          message: '',
+        },
+        firstname: {
+          valid: false,
+          value: undefined,
+          message: '',
+        },
+        lastname: {
+          valid: false,
+          value: undefined,
+          message: '',
+        },
+        emailaddress: {
+          valid: false,
+          value: undefined,
+          message: '',
+        },
+        postcode: {
+          valid: false,
+          value: undefined,
+          message: '',
+        },
+        address1: {
+          valid: false,
+          value: undefined,
+          message: '',
+        },
+        address2: {
+          valid: true,
+          value: undefined,
+          message: '',
+        },
+        address3: {
+          valid: true,
+          value: undefined,
+          message: '',
+        },
+        town: {
+          valid: false,
+          value: undefined,
+          message: '',
+        },
+        country: {
+          valid: false,
+          value: undefined,
+          message: '',
+        },
+      },
+    };
+    // Put the field refs from children into an array
+    const refs = [];
+    this.setRef = (element) => {
+      if (element) {
+        // fields from postcode lookup
+        if (element.fieldRefs) {
+          element.fieldRefs.forEach(item => refs.push(item));
+        } else {
+          // remaining input fields
+          refs.push(element.inputRef);
+        }
+        this.fieldRefs = refs;
+      }
+    };
+  }
+
+  /**
+   * Merges any overrides to the default input field data json with it
+   */
+  componentWillMount() {
+    this.setState({
+      inputFieldProps: this.mergeInputFieldProps(defaultInputFieldsData),
+    });
+  }
+
+  /**
+   * Deals with component update after pressing submit button
+   */
+  componentDidUpdate() {
+    if (this.state.showErrorMessages === true && this.state.formValidity === false) {
+      // timeout needed for error class names to appear
+      scrollTimeout = setTimeout(() => { this.scrollToError(); }, 500);
+      this.setErrorMessagesToFalse();
+    }
+    if (this.state.showErrorMessages === false && this.state.formValidity === true) {
+      this.submitForm();
+    }
+  }
+
+  /**
+   * Gets the timestamp and formats it
+   * @return string
+   */
+  getTimestamp() {
+    const getTimeStamp = Math.round((new Date()).getTime() / 1000);
+    const timestamp = new Date(getTimeStamp * 1000);
+    return timestamp;
+  }
+
+  /**
+   * Gets the campaign name based on the url
+   * @param url
+   * @return {*}
+   */
+  getCampaign(url) {
+    let campaign;
+    if (url.includes('sportrelief')) {
+      campaign = 'SR18';
+    } else if (url.includes('rednoseday')) {
+      campaign = 'RND19';
+    } else {
+      campaign = 'CR';
+    }
+    return campaign;
+  }
+
+  /**
+   * Gets the current hostname.
+   * Replaces 'localhost' to a default or uses the browser's current url.
+   * @return string
+   */
+  getCurrentUrl() {
+    let url = null;
+    if (window.location.hostname === 'localhost') {
+      url = 'http://local.comicrelief.com';
+    } else {
+      url = window.location.href;
+    }
+    return url;
+  }
+
+  /**
+   * Updates validation state
+   * @param name
+   * @param valid
+   */
+  setValidity(name, valid) {
+    if (name && valid) {
+      this.setState((prevState) => {
+        let newState;
+        if (prevState.validation[name] !== undefined &&
+          (prevState.validation[name].value === undefined ||
+            prevState.validation[name].value !== valid.value)) {
+          newState = {
+            ...this.state,
+            validation: {
+              ...this.state.validation,
+              [name]: valid,
+            },
+          };
+        }
+        return newState;
+      });
+    }
+  }
+
+  setErrorMessagesToFalse() {
+    this.setState({
+      ...this.state,
+      showErrorMessages: false,
+    });
+  }
+
+  /**
+   * Goes through field refs, gets the first erroring field and focuses on it.
+   * If inputelement.labels is not supported: scrolls form into view
+   */
+  scrollToError() {
+    let item;
+    for (let i = 0; i <= this.fieldRefs.length; i += 1) {
+      if (this.fieldRefs[i].labels !== undefined) {
+        const classes = this.fieldRefs[i].labels[0].getAttribute('class');
+        if (classes.includes('error')) {
+          item = this.fieldRefs[i];
+          item.labels[0].scrollIntoView('smooth');
+          item.focus();
+          break;
+        }
+      } else {
+        document.querySelector('form').scrollIntoView();
+        break;
+      }
+    }
+    clearTimeout(scrollTimeout);
+  }
+
+  /**
+   * Maps the input field properties to a new array containing the input field instances
+   * @returns {Array}
+   */
+  createInputFields() {
+    const inputFields = [];
+    console.log('this.state.inputFieldProps', this.state.inputFieldProps);
+    Object.entries(this.state.inputFieldProps).map(([field, props]) => inputFields.push(<InputField
+      ref={this.setRef}
+      key={field}
+      id={props.id}
+      type={props.type}
+      name={props.name}
+      label={props.label}
+      required={props.required}
+      pattern={props.pattern}
+      placeholder={props.placeholder}
+      min={props.min}
+      max={props.max}
+      defaultChecked={props.defaultChecked}
+      helpText={props.helpText}
+      emptyFieldErrorText={props.emptyFieldErrorText}
+      invalidErrorText={props.invalidErrorText}
+      setBackgroundColor={props.type === 'checkbox'}
+      additionalText={props.additionalText}
+      showErrorMessage={this.state.showErrorMessages}
+      isValid={(valid, name) => { this.setValidity(name, valid); }}
+    />));
+    return inputFields;
+  }
+
+  /**
+   * Merge default input field properties with overrides.
+   * @returns {object}
+   */
+  mergeInputFieldProps(defaultInputFieldsProps) {
+    const inputFields = defaultInputFieldsProps;
+    const overrides = this.props.inputFieldOverrides;
+    Object.entries(overrides).forEach(([key]) => {
+      Object.assign(inputFields[key], overrides[key]);
+    });
+    return inputFields;
+  }
+  /**
+   * Creates formValues object and submits form
+   */
+  submitForm() {
+    const url = this.getCurrentUrl();
+    const campaign = this.getCampaign(url);
+    // required settings to post to api endpoint
+    const settings = {
+      campaign,
+      transSource: `${campaign}_GiftAid`,
+      transSourceUrl: url,
+      transType: 'GiftAid',
+      timestamp: this.getTimestamp(),
+    };
+
+    // create field values
+    const fieldValues = {};
+    Object.keys(this.state.validation).forEach((key) => {
+      let value = this.state.validation[key].value;
+      // todo deal with the value output in InputField component
+      if (key === 'confirm') {
+        value = this.state.validation[key].value === true ? 1 : 0;
+      }
+      fieldValues[key] = value;
+    });
+
+    // Combine all form data and settings
+    const formValues = Object.assign({}, fieldValues, settings);
+
+    // post form data and settings to endpoint
+    axios.post(ENDPOINT_URL, formValues)
+      .then(() => {
+        this.props.history.push({
+          pathname: '/success',
+          state: { firstname: formValues.firstname },
+        });
+      })
+      .catch(() => {
+        this.props.history.push({
+          pathname: '/sorry',
+        });
+      });
+  }
+
+  /**
+   * Checks if any field is invalid.
+   * If invalid fields: shows error sets state to show errorMessages.
+   * If all fields valid: sets form validity to true
+   * @param e
+   */
+  validateForm(e) {
+    e.preventDefault();
+    // Put field validation into new array to check for invalid fields
+    const fields = [];
+    Object.keys(this.state.validation).map(key =>
+      fields.push(this.state.validation[key].valid));
+    // values can be null or empty strings so check for not true
+    const invalidFields = fields.some(element => element !== true);
+    // update state accordingly
+    if (invalidFields === false) {
+      this.setState({
+        ...this.state,
+        formValidity: true,
+      });
+    }
+    if (invalidFields === true) {
+      this.setState({
+        ...this.state,
+        formValidity: false,
+        showErrorMessages: true,
+      });
+    }
+  }
+
+  /**
+   * Renders out the just in time message
+   */
+  renderJustInTimeMessage() {
+    const justInTimeLinkText = 'Why do we collect this info?';
+    return (
+      <JustInTime linkText={justInTimeLinkText}>
+        <p>
+          <strong>Name, email and billing address: </strong>
+          we need it to create a receipt for your payment and send it to you.
+        </p>
+        <p>
+          <strong>Phone number:</strong> we collect it in case there is an issue
+          with gift aid donation.
+        </p>
+      </JustInTime>
+    );
+  }
   renderFormHeader() {
     return (
       <div>
@@ -21,34 +373,78 @@ class Update extends Component {
           </p>
           :
           null }
-
       </div>
     );
   }
 
+
   render() {
+    const { formDataSuccess, formDataError } = this.state;
     return (
       <main role="main">
         <section>
           <form
-            id="form--update"
+            id="form"
             noValidate
-            className="update-giftaid__form"
+            className="giftaid__form"
+            data-success={formDataSuccess}
+            data-error={formDataError}
           >
             {this.renderFormHeader()}
-            {/* { this.createInputFields() } */}
+
+            <h3 className="form--update__title text-align-centre">
+              Who is changing their declaration?
+            </h3>
+
+            { this.createInputFields() }
+            <PostcodeLookup
+              ref={this.setRef}
+              label="Postal address"
+              showErrorMessages={this.state.showErrorMessages}
+              isAddressValid={
+                (validation) => {
+                  Object.keys(validation).map(key => this.setValidity(key, validation[key]));
+                }
+              }
+            />
+
+            {/* Broken-out from the inputfield array to position it here */}
+            <InputField
+              ref={this.setRef}
+              key="giftaid"
+              id="giftaid"
+              type="checkbox"
+              name="confirm"
+              label="Yes, I would like Comic Relief to claim Gift Aid on my donation *"
+              required="false"
+              emptyFieldErrorText="To Gift Aid your donation you need to tick the checkbox"
+              additionalText="* By ticking I state I am a UK taxpayer making a personal donation and understand that if I pay less Income Tax and/or Capital Gains Tax than the amount of Gift Aid claimed on all my donations, it is my responsibility to pay any difference. <a href='https://www.comicrelief.com/frequently-asked-questions' class='link inline' target='_blank'>Find out more</a>"
+            />
+
             <button
               type="submit"
               className="btn btn--red"
-              /* onClick={e => this.validateForm(e)} */
-            >Gift Aid your donation
+              onClick={e => this.validateForm(e)}
+            >Update Gift Aid Declaration
             </button>
-            {/* {this.renderJustInTimeMessage() */ }
+            {this.renderJustInTimeMessage()}
           </form>
         </section>
       </main>
     );
   }
 }
+
+Update.defaultProps = {
+  inputFieldOverrides: {},
+  history: { push: { } },
+};
+
+Update.propTypes = {
+  inputFieldOverrides: propTypes.shape(propTypes.shape),
+  history: propTypes.shape({
+    push: propTypes.func,
+  }),
+};
 
 export default Update;
