ec0f5e544cee39cecea4a7b8f719581a542ea04d
krupa <k.pammi@comicrelief.com>
2018-10-17 15:52:34 +0100

    tests for giftaid update

---
+++ tests/commands/updateGA.js
@@ -0,0 +1,20 @@
+const commands = {
+  fillFormUpdateYes: function (client) {
+    return client
+      .setValue('#field-input--firstname', 'test')
+      .setValue('#field-input--lastname', 'user')
+      .setValue('#field-input--emailaddress', 'test@comicrelief.com')
+      .setValue('#field-input--postcode', 'se1 7tp')
+      .click('a[aria-describedby=field-error--addressDetails]')
+      .pause(200)
+      .setValue('#field-input--address1', '21 test road')
+      .setValue('#field-input--town', 'London')
+      .click('input[type="radio"][value="1"]')
+      .click('button[type=submit]')
+      .pause(5000);
+    },
+};
+
+module.exports = {
+    fillFormUpdateYes: commands.fillFormUpdateYes,
+};
--- tests/feature/giftaid-sanity/e2e.js
+++ tests/feature/giftaid-sanity/e2e.js
@@ -6,11 +6,91 @@ module.exports = {
     'User completes main giftaid journey': function (client) {
         client.url(process.env.BASE_URL).maximizeWindow().waitForElementVisible('body', 1000);
 
-        client.page.test().fillForm(client);
+        client.page.mainGA().fillForm(client);
 
         client.waitForElementVisible('div.success-wrapper--inner > h1', 1000);
 
         client.expect.element('div > div.success-wrapper--inner > h1').text.to.equal('Thank you,\n' + 'test!');
+
+        client.end();
+    },
+
+    'User completes giftaid update journey': function (client) {
+        client.url(process.env.BASE_URL + 'update').maximizeWindow().waitForElementVisible('body', 1000);
+
+        client.setValue('#field-input--transactionId', '3D487A59-716B-440D-BD43-50ED301DD9BA');
+
+        client.page.updateGA().fillFormUpdateYes(client);
+
+        client.waitForElementVisible('div.success-wrapper--inner > div > h1', 1000);
+
+        client.expect.element('div.success-wrapper--inner > div > h1').text.to.equal('Thank you,\n' + 'test!');
+
+        client.end();
+    },
+
+    'User completes giftaid update journey from sms': function (client) {
+        client.url(process.env.BASE_URL + 'update/transID').maximizeWindow().waitForElementVisible('body', 1000);
+
+        client.click('input[type="radio"][value="sms"]');
+
+        client.page.updateGA().fillFormUpdateYes(client);
+
+        client.waitForElementVisible('div.success-wrapper--inner > div > h1', 1000);
+
+        client.expect.element('div.success-wrapper--inner > div > h1').text.to.equal('Thank you,\n' + 'test!');
+
+        client.end();
+    },
+
+    'User completes giftaid update journey from online': function (client) {
+        client.url(process.env.BASE_URL + 'update/transID').maximizeWindow().waitForElementVisible('body', 1000);
+
+        client.click('input[type="radio"][value="online"]');
+
+        client.page.updateGA().fillFormUpdateYes(client);
+
+        client.waitForElementVisible('div.success-wrapper--inner > div > h1', 1000);
+
+        client.expect.element('div.success-wrapper--inner > div > h1').text.to.equal('Thank you,\n' + 'test!');
+
+        client.end();
+    },
+
+    'User completes giftaid update journey from call centre': function (client) {
+        client.url(process.env.BASE_URL + 'update/transID').maximizeWindow().waitForElementVisible('body', 1000);
+
+        client.click('input[type="radio"][value="callcentre"]');
+
+        client.page.updateGA().fillFormUpdateYes(client);
+
+        client.waitForElementVisible('div.success-wrapper--inner > div > h1', 1000);
+
+        client.expect.element('div.success-wrapper--inner > div > h1').text.to.equal('Thank you,\n' + 'test!');
+
+        client.end();
+    },
+
+    'User selects "no" for giftaid update declaration': function (client) {
+        client.url(process.env.BASE_URL + 'update').maximizeWindow().waitForElementVisible('body', 1000);
+
+        client.setValue('#field-input--transactionId', 'D-BEX1501');
+
+        client.page.updateNo().fillFormUpdateNo(client);
+
+        client.waitForElementVisible('div.success-wrapper--inner > div > h1', 1000);
+
+        client.expect.element('div.success-wrapper--inner > div > h1').text.to.equal('Thanks for letting us know');
+
+        client.end();
+    },
+
+    'Verify sorry page': function (client) {
+        client.url(process.env.BASE_URL + 'sorry').maximizeWindow().waitForElementVisible('body', 1000);
+
+        client.waitForElementVisible('div > h1', 1000);
+
+        client.expect.element('div > h1').text.to.equal('Sorry!');
 
         client.end();
     },
