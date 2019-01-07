6ed5a71d7f7eaf495349d08f542dbd4c4affa099
Sidney Barrah <sidney3659@c02vh07gh2f.vxh.comicrelief.org.uk>
2019-01-07 12:00:42 +0000

    Bump dependencies

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -324,13 +324,13 @@ new webpack.DefinePlugin(env.stringified),
   // You can remove this if you don't use Moment.js:
   new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
 ],
-// Some libraries import Node modules but don't use them in the browser.
-// Tell Webpack to provide empty mocks for them so importing them works.
-node: {
-  dgram: 'empty',
+  // Some libraries import Node modules but don't use them in the browser.
+  // Tell Webpack to provide empty mocks for them so importing them works.
+  node: {
+    dgram: 'empty',
     fs: 'empty',
     net: 'empty',
     tls: 'empty',
     child_process: 'empty',
-},
+  },
 };
