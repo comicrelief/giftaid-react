56ad7855eb50f6da9b25bd2213d34615be60b7a2
Heleen3258 <h.mol@comicrelief.com>
2019-01-08 12:34:48 +0000

    cypress fixes for success page

--- cypress/integration/mainGA_spec.js
+++ cypress/integration/mainGA_spec.js
@@ -83,6 +83,6 @@ describe('e2e test', () => {
     it('verify success page', () => {
       console.log(firstName);
         cy.get('button[type=submit]').click().url('/success')
-        cy.contains(`Thank you, ${firstName}!`)
+        cy.get('.success-wrapper').should('contain', 'Thank you,').and('contain', `${firstName}!`)
     })
 })
