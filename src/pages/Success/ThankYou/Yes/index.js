a874edba5893d92c001586c2233e3ea9f8c80bec
Sidney Barrah <neothedeveloper231@gmail.com>
2019-06-10 15:00:30 +0100

    Refactored Success ThankYou component

---
+++ src/pages/Success/ThankYou/Yes/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Yes = (props) => {
+
+  return (
+    <div>
+      <h1>Thank you, <br /> {props.firstname}!</h1>
+      <p>
+        We’ve registered your Gift Aid declaration,
+        we’ll use it to pay for our operational costs.
+      </p>
+    </div>
+  );
+
+};
+
+export default Yes;
--- src/pages/Success/ThankYou/index.js
+++ src/pages/Success/ThankYou/index.js
@@ -3,33 +3,28 @@ import React, {useContext} from 'react';
 // import context
 import AppContext from '../../../context/AppContext';
 
+import Yes from './Yes';
+import No from './No';
+
+
 const ThankYou = (props) => {
 
   // get context
   const { successState: state } = useContext(AppContext);
 
-  if (state !== undefined && state.firstname !== undefined && state.giftAidChoice !== '0') {
-    return (
-      <div>
-        <h1>Thank you, <br /> {state.firstname}!</h1>
-        <p>
-          We’ve registered your Gift Aid declaration,
-          we’ll use it to pay for our operational costs.
-        </p>
-      </div>
-    );
-  }
-  if (state !== undefined && state.giftAidChoice === '0') {
-    return (
-      <div>
-        <h1>Thanks for letting us know</h1>
-        <p>
-          We won’t claim Gift Aid for your donation
-        </p>
-      </div>
-    );
-  }
-  return null;
+  return (
+    <React.Fragment>
+      { state !== undefined && state.firstname !== undefined && state.giftAidChoice !== '0'
+        ?
+        <Yes
+          firstname={state.firstname}
+        />
+        :
+        <No/>
+      }
+    </React.Fragment>
+
+  );
 };
 
 export default ThankYou;
