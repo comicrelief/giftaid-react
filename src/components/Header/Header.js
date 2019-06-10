3400152ada6b37ce8194830eed0910cd0355ec4f
Sidney Barrah <neothedeveloper231@gmail.com>
2019-06-10 14:28:38 +0100

    Added logo component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import logo from './images/CR18-logo.svg';
 
+import Logo from './Logo';
+
 const Header = (props) => {
 
   return (
     <header className="header" role="banner">
       <div className="header__inner-wrapper">
         <div id="block-branding" className="block block-branding">
-          <a href="https://www.comicrelief.com" className="site-logo" title="Comic Relief" rel="home noopener noreferrer">
-            <img src={logo} alt="Go to Comic Relief" />
-          </a>
+          <Logo
+            href="https://www.comicrelief.com"
+            className="site-logo"
+            title="Comic Relief"
+            rel="home noopener noreferrer"
+            logo={logo}
+            alt="Go to Comic Relief"
+          />
         </div>
       </div>
     </header>
