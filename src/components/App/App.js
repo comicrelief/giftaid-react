64c36e3ec27aa23762a45d2f9f0b21c5973a5960
Peter Vanhee <peter.vanhee@gmail.com>
2019-12-06 15:25:49 +0000

    change to canonical url

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -35,7 +35,7 @@ function App (props) {
     firstname: undefined,
     giftAidChoice: undefined,
   };
-  
+
   const [successState, setSuccessState] = useState(initialState); // initialise successState state
 
   /**
@@ -91,7 +91,7 @@ function App (props) {
         </title>
         <meta name="description" content={site.get('meta').description} />
         <meta property="og:title" content="Gift Aid your donation" />
-        <meta property="og:image" content={`https://giftaid.sportrelief.com/images/thank-you-mob.jpg`} />
+        <meta property="og:image" content="https://giftaid.comicrelief.com/images/gift-aid-logo.png" />
         <meta property="og:site_name" content="Comic Relief" />
         <meta property="og:url" content={window.location.href} />
         <meta property="og:description" content={site.get('meta').description} />
