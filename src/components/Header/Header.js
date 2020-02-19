98739b24718f2d1146f3a5951be98094140f0376
louiaR <louitiani@gmail.com>
2020-02-19 16:00:21 +0000

    Conditionally display logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './images/CR18-logo.svg';
 
 import Logo from './Logo';
+import { url } from '../../pages/GiftAid/utils/Utils';
+import SiteConfig from '../../config/site.json';
 
-const Header = (props) => {
-
-  return (
-    <header className="header" role="banner">
-      <div className="header__inner-wrapper">
-        <div id="block-branding" className="block block-branding">
-          <Logo
-            href="https://www.comicrelief.com"
-            className="site-logo"
-            title="Comic Relief"
-            rel="home noopener noreferrer"
-            logo={logo}
-            alt="Go to Comic Relief"
-          />
-        </div>
-      </div>
-    </header>
-  );
+const Header = () => {
+	const isCR = url.includes('comic');
+	return (
+		<header className='header' role='banner'>
+			<div className='header__inner-wrapper'>
+				<div id='block-branding' className='block block-branding'>
+					<Logo
+						href={
+							isCR ? SiteConfig.CRGIFTAID.logo : SiteConfig.SRGIFTAID.home_url
+						}
+						className='site-logo'
+						title='Comic Relief'
+						rel='home noopener noreferrer'
+						logo={isCR ? SiteConfig.CRGIFTAID.logo : SiteConfig.SRGIFTAID.logo}
+						alt={isCR ? SiteConfig.CRGIFTAID.alt : SiteConfig.SRGIFTAID.alt}
+					/>
+				</div>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
