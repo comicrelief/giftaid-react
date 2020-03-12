e5402b909e18473f04a563febfb2e93b152bfe9a
louiaR <louitiani@gmail.com>
2020-03-12 10:52:52 +0000

    Trigger news build

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -85,7 +85,8 @@ function App(props) {
 	const isSRCampaign = true;
 
 	const copy = isSRCampaign
-		? `Sport Relief is an initiative of Comic Relief. Comic Relief is the trading name of Charity Projects, a registered charity in England and Wales (326568) and Scotland (SC039730), which is a company limited by
+		? `Sport Relief is an initiative of Comic Relief. Comic Relief is the trading name of Charity Projects, a registered charity in England and Wales (326568) and Scotland (SC039730),
+		which is a company limited by
 	guarantee registered in England and Wales (01806414). Registered address: 89 Albert Embankment, London SE1 7TP.`
 		: 'Comic Relief is the trading name of Charity Projects, a registered charity in England and Wales (326568) and Scotland (SC039730), which is a company limited by guarantee registered in England and Wales (01806414). Registered address: 1st Floor, 89 Albert Embankment, London, SE1 7TP.';
 
