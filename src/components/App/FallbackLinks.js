135dd082f03127245f278fb1d3cbdd041e5ccd81
louiaR <louitiani@gmail.com>
2020-04-06 13:00:09 +0100

    Add fallback menu links

---
+++ src/components/App/FallbackLinks.js
@@ -0,0 +1,48 @@
+const fallbackFooterMenuCR = [
+	{
+		url: 'https://lite.comicrelief.com/legal/privacy-notice',
+		title: 'Privacy notice',
+	},
+	{
+		url: 'https://lite.comicrelief.com/legal/',
+		title: 'Legal',
+	},
+];
+
+const fallbackFooterMenuSR = [
+	{
+		url: 'https://lite.sportrelief.com/terms-of-use',
+		title: 'Legal',
+	},
+	{
+		url: 'https://lite.sportrelief.com/privacy-notice',
+		title: 'Privacy notice',
+	},
+];
+
+const fallbackFooterMenuBN = [
+	{
+		url: 'https://www.comicrelief.com/big-night-in-contact-us',
+		title: 'Contact us',
+	},
+	{
+		url: 'https://www.comicrelief.com/big-night-in-legal',
+		title: 'Legal',
+	},
+	{
+		url: 'https://www.comicrelief.com/big-night-in-privacy',
+		title: 'Privacy',
+	},
+];
+
+const getFallbackMenuItems = (url) => {
+	switch (url) {
+		case url.includes('sportrelief'):
+			return fallbackFooterMenuSR;
+		case url.includes('bignightin'):
+			return fallbackFooterMenuBN;
+		default:
+			return fallbackFooterMenuCR;
+	}
+}
+export default getFallbackMenuItems;
