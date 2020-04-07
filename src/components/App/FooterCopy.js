b7a4ea6456ad5bf79f0023e921392f7188ae740f
Andy E Phipps <a.phipps@comicrelief.com>
2020-04-07 15:37:29 +0100

    Fix formatting on footer copy

--- src/components/App/FooterCopy.js
+++ src/components/App/FooterCopy.js
@@ -1,21 +1,21 @@
 const FooterCopy = {
 
-    SR: `Sport Relief is an initiative of Comic Relief. Comic Relief is the trading name of Charity Projects, 
-    a registered charity in England and Wales (326568) and Scotland (SC039730), which is a company limited by
-        guarantee registered in England and Wales (01806414). Registered address: 89 Albert Embankment, London SE1 7TP.`,
+    SR: 'Sport Relief is an initiative of Comic Relief. Comic Relief is the trading name of Charity Projects, ' +
+    'a registered charity in England and Wales (326568) and Scotland (SC039730), which is a company limited by' +
+        'guarantee registered in England and Wales (01806414). Registered address: 89 Albert Embankment, London SE1 7TP.',
   
-    CR: `Comic Relief is the trading name of Charity Projects, a registered charity in England and Wales 
-    (326568) and Scotland (SC039730), which is a company limited by guarantee registered in England and Wales 
-    (01806414). Registered address: 1st Floor, 89 Albert Embankment, London, SE1 7TP.`,
+    CR: 'Comic Relief is the trading name of Charity Projects, a registered charity in England and Wales ' +
+    '(326568) and Scotland (SC039730), which is a company limited by guarantee registered in England and Wales ' +
+    '(01806414). Registered address: 1st Floor, 89 Albert Embankment, London, SE1 7TP.',
   
-    BIGNIGHTIN: `Big Night In is a joint initiative between BBC Children in Need and Comic Relief. 
-    This website and donation page is managed by Comic Relief for The Big Night In.
-    BBC CHILDREN IN NEED incorporated and registered in England and Wales with Company number
-     04723022 and Registered charity no. 802052 in England and Wales and SC039557 in Scotland whose 
-     registered office is at Bridge House, Media City UK, Salford, England, M50 2BH
-    Comic Relief is the trading name of Charity Projects, a registered charity in England and 
-    Wales (326568) and Scotland (SC039730), which is a company limited by guarantee registered in England and Wales (01806414). 
-    Registered address: 1st Floor, 89 Albert Embankment, London, SE1 7TP.`,
+    BIGNIGHTIN: 'Big Night In is a joint initiative between BBC Children in Need and Comic Relief. ' +
+    'This website and donation page is managed by Comic Relief for The Big Night In. ' +
+    'BBC CHILDREN IN NEED incorporated and registered in England and Wales with Company number ' +
+    '04723022 and Registered charity no. 802052 in England and Wales and SC039557 in Scotland whose ' +
+    'registered office is at Bridge House, Media City UK, Salford, England, M50 2BH. ' +
+    'Comic Relief is the trading name of Charity Projects, a registered charity in England and ' +
+    'Wales (326568) and Scotland (SC039730), which is a company limited by guarantee registered in England and Wales (01806414). ' +
+    'Registered address: 1st Floor, 89 Albert Embankment, London, SE1 7TP.',
   };
   
   export default FooterCopy;
