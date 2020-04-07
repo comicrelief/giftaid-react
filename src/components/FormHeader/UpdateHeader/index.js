b45ba8dc9073ac3df4c25ef68d7e9f695c263835
Mohamed Labib <lebaz20@gmail.com>
2020-04-07 10:34:20 +0200

    Use bigNightIn copy in the corresponding domain only

--- src/components/FormHeader/UpdateHeader/index.js
+++ src/components/FormHeader/UpdateHeader/index.js
@@ -1,10 +1,21 @@
 import React, { useContext } from 'react';
 
 import FormContext from "../../../context/FormContext";
+import SiteService from '../../../service/Site.service';
 
 const UpdateHeader = (props) => {
 
   const { urlTransactionId } = useContext(FormContext); // get states from context
+  const site = new SiteService();
+  let claimCopy = null;
+  switch(site.getSite()) {
+    case 'BIGNIGHTIN':
+      claimCopy = ' the Government gives back 25% of their value. This will be split 50/50 between Comic Relief and BBC Children in Need as per your donation.';
+    break;
+    default:
+      claimCopy = ' the Government gives us back 25% of their value.';
+    break;
+  };
 
   return (
     <React.Fragment>
@@ -12,8 +23,8 @@ const UpdateHeader = (props) => {
         <div>Edit your Gift Aid declaration</div>
       </h2>
       <p className="text-align-centre">
-        We can claim Gift Aid from personal donations made by UK taxpayers:
-        the Government gives back 25% of their value. This will be split 50/50 between Comic Relief and BBC Children in Need as per your donation.
+        We can claim Gift Aid from personal donations made by UK taxpayers: 
+        {claimCopy}
       </p>
       {typeof urlTransactionId !== 'undefined' && urlTransactionId !== null ?
         <p className="text-align-centre transaction-id">
