6f3778a9df0cad99575dcc73d0c41345129dff9f
Krupa Pammi <k.pammi@comicrelief.com>
2021-05-19 15:36:05 +0100

    update tests with MP changes

--- tests/feature/update/validSubmission.js
+++ tests/feature/update/validSubmission.js
@@ -2,7 +2,7 @@ const { v4: uuidv4 } = require('uuid');
 
 module.exports = {
 
- '@tags': ['sanity', 'update'],
+ '@tags': ['sanity', 'update', 'validSubmission'],
 
   'Verify all elements present on giftaid update': function (client) {
     client.url(process.env.BASE_URL + 'update').maximizeWindow().waitForElementVisible('body', 1000);
@@ -20,22 +20,6 @@ module.exports = {
       .assert.elementPresent('#field-select--country')
       .assert.elementPresent('#form > button')
       .assert.elementPresent('.form__row--just-in-time-block>div>a');
-    client.end();
-  },
-
-  'Verify error messages on Giftaid update form': function (client) {
-    client.url(process.env.BASE_URL + 'update').maximizeWindow().waitForElementVisible('body', 1000);
-    client
-      .click('button[type=submit]')
-      .assert.containsText('#field-error--transactionId>span', 'Please fill in your transaction id')
-      .assert.containsText('#field-error--firstname>span', 'Please fill in your first name')
-      .assert.containsText('#field-error--lastname>span', 'Please fill in your last name')
-      .assert.containsText('#field-error--postcode>span', 'Please enter your postcode')
-      .assert.containsText('#field-error--addressDetails>span', 'Please fill in your address')
-      .click('a[aria-describedby=field-error--addressDetails]')
-      .assert.containsText('#field-error--address1>span', 'Please fill in your address line 1')
-      .assert.containsText('#field-error--town>span', 'Please fill in your town/city')
-      .assert.containsText('#field-error--giftAidClaimChoice>span','This field is required');
     client.end();
   },
 
