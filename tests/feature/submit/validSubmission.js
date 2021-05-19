4b2d26c7814e1390a384d550bb5dd290fa144c2f
Krupa Pammi <k.pammi@comicrelief.com>
2021-05-19 16:33:04 +0100

    remove double submission

--- tests/feature/submit/validSubmission.js
+++ tests/feature/submit/validSubmission.js
@@ -16,8 +16,6 @@ module.exports = {
   'User completes main giftaid journey from link with token': function (client) {
     client.url(process.env.BASE_URL + 'uX8R5SzcKfk=').maximizeWindow().waitForElementVisible('body', 1000);
     client.page.form().fillFormPrefilledMobile(client);
-    client.click('button[type=submit]');
-    client.pause(5000);
     client.waitForElementVisible('div > h1', 2000);
     client.expect.element('div > h1').text.to.equal('Thank you,\n' +
       'test!');
