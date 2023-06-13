906c0c1823daf057c0ab2804f1a70b3b20f88417
Krupa Pammi <krupa.pammi@gmail.com>
2023-06-13 08:47:47 +0100

    test: add giftaid playwright tests (#392)
    * test: add giftaid playwright tests
    
    * add node development env var in playwright runner step
    
    * debug
    
    * debug: remove restore caches step
    
    * debug: add npm login step
    
    * debug
    
    * debug: update node version to 14.19.1
    
    * debug: update node version to 14.19.1 in playwright job
    
    * change node-modules back to version 14
    
    * Force rebuild of node-sass due to platform differences
    
    * enable retry option
    
    * lock playwright version to 1.33.0 as anything above doesn't support node 14
    
    * remove cypress module, config and tests
    
    ---------
    
    Co-authored-by: AndyEPhipps <a.phipps@comicrelief.com>

---
+++ e2e/submit/formValidation.spec.js
@@ -0,0 +1,176 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+const { Commands } = require('../utils/commands');
+
+test.describe('Address validation', () => {
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/', { timeout: 30000 });
+
+    await page.waitForLoadState('domcontentloaded');
+
+    await page.waitForSelector('#field-label--giftaid');
+    await page.locator('#field-label--giftaid').click();
+
+  });
+
+  test('clearing input fields should show error messages', async ({ page }) => {
+    // empty phone number field
+    await page.locator('#field-input--mobile').fill('07123456789');
+    await page.locator('#field-input--mobile').fill('');
+    await expect(page.locator('div#field-error--mobile > span')).toContainText('Please fill in your mobile number');
+
+    // empty first name field
+    await page.locator('input#field-input--firstname').fill('test');
+    // clear firstname
+    await page.locator('input#field-input--firstname').fill('');
+    await expect(page.locator('div#field-error--firstname > span')).toContainText('Please fill in your first name');
+
+    // empty lastname field
+    await page.locator('input#field-input--lastname').fill('user');
+    // clear lastname
+    await page.locator('input#field-input--lastname').fill('');
+    await expect(page.locator('div#field-error--lastname > span')).toContainText('Please fill in your last name');
+
+    // empty postcode field
+    await page.locator('input#field-input--postcode').fill('SE17TP');
+    // clear postcode
+    await page.locator('input#field-input--postcode').fill('');
+    await expect(page.locator('div#field-error--postcode > span')).toContainText('Please enter your postcode');
+
+    await page.close();
+  });
+
+  test('validate mobile number field', async ({ page }) => {
+
+    const commands = new Commands(page);
+
+    await page.locator('#field-input--mobile').fill('07123456789');
+    await page.locator('#field-input--mobile').fill('');
+    await expect(page.locator('div#field-error--mobile > span')).toContainText('Please fill in your mobile number');
+
+    // mobile number less than 11 digits shows error message
+    await page.locator('input#field-input--mobile').type('0712345678', {delay: 100});
+    await page.waitForSelector('div#field-error--mobile > span');
+    await expect(page.locator('div#field-error--mobile > span')).toContainText('Please enter a valid mobile phone number - it must be the same number associated with your donation.');
+
+    // mobile number more than 11 digits shows error message
+    await page.locator('input#field-input--mobile').fill(''); // clear the mobile number field
+    await page.locator('input#field-input--mobile').type('0712345678900', {delay: 100});
+    await page.waitForSelector('div#field-error--mobile > span');
+    await expect(page.locator('div#field-error--mobile > span')).toContainText('Please enter a valid mobile phone number - it must be the same number associated with your donation.');
+
+    // mobile number with spaces should show error
+    await page.locator('input#field-input--mobile').fill(''); // clear the mobile number field
+    await page.locator('input#field-input--mobile').type('0712 345 6789', {delay: 100});
+    await page.waitForSelector('div#field-error--mobile > span');
+    await expect(page.locator('div#field-error--mobile > span')).toContainText('Please enter a valid mobile phone number - it must be the same number associated with your donation.');
+
+    // mobile number with alphabetical chars should show error
+    await page.locator('input#field-input--mobile').fill(''); // clear the mobile number field
+    await page.locator('input#field-input--mobile').type('0780ab5694245', {delay: 100});
+    await page.waitForSelector('div#field-error--mobile > span');
+    await expect(page.locator('div#field-error--mobile > span')).toContainText('Please enter a valid mobile phone number - it must be the same number associated with your donation.');
+
+    // mobile number starting with alphabetical chars should show error
+    await page.locator('input#field-input--mobile').fill(''); // clear the mobile number field
+    await page.locator('input#field-input--mobile').type('abcv07805694245', {delay: 100});
+    await page.waitForSelector('div#field-error--mobile > span');
+    await expect(page.locator('div#field-error--mobile > span')).toContainText('Please enter a valid mobile phone number - it must be the same number associated with your donation.');
+
+    // mobile number ending with alphabetical chars should show error
+    await page.locator('input#field-input--mobile').fill(''); // clear the mobile number field
+    await page.locator('input#field-input--mobile').type('07805694245dfef', {delay: 100});
+    await page.waitForSelector('div#field-error--mobile > span');
+    await expect(page.locator('div#field-error--mobile > span')).toContainText('Please enter a valid mobile phone number - it must be the same number associated with your donation.');
+
+    // clear the mobile number field
+    await page.locator('input#field-input--mobile').fill(''); // clear the mobile number field
+
+    // entering valid input fields should be able to submit the form
+    await commands.populateFormFields();
+
+    // submit the form
+    await page.locator('button[type=submit]').click();
+
+    await expect(page.locator('div > h1')).toContainText('Thank you,\n' +
+      'test!');
+
+    await page.close();
+  });
+
+  test('validate first name field', async ({ page }) => {
+
+    const commands = new Commands(page);
+
+    await page.locator('#field-input--firstname').fill('test');
+    await page.locator('#field-input--firstname').fill('');
+    await expect(page.locator('#field-error--firstname')).toContainText('Please fill in your first name');
+
+    // enter firstname field with special chars should show error message
+    await page.locator('#field-input--firstname').type('Test^$%£');
+    await expect(page.locator('#field-error--firstname')).toContainText('This field only accepts 25 alphabetic characters and \' - starting with alphabetic characters');
+
+    // firstname with just a space should show error message
+    await page.keyboard.press('Backspace');
+    await page.locator('#field-input--firstname').type(' ');
+    await expect(page.locator('#field-error--firstname')).toContainText('This field only accepts 25 alphabetic characters and \' - starting with alphabetic characters');
+
+    // firstname with a mixture of alphanumeric chars should show error message
+    await page.locator('#field-input--firstname').fill(''); // clear the first-name field
+    await page.locator('#field-input--firstname').type('123Test');
+    await expect(page.locator('#field-error--firstname')).toContainText('This field only accepts 25 alphabetic characters and \' - starting with alphabetic characters');
+
+    // clear the first name field
+    await page.locator('#field-input--firstname').fill('');
+
+    // entering valid input fields should be able to submit the form
+    await commands.populateFormFields();
+
+    // submit the form
+    await page.locator('button[type=submit]').click();
+
+    await expect(page.locator('div > h1')).toContainText('Thank you,\n' +
+      'test!');
+
+    await page.close();
+  });
+
+  test('validate last name field', async ({ page }) => {
+
+    const commands = new Commands(page);
+
+    await page.locator('#field-input--lastname').fill('test lastname');
+    await page.locator('#field-input--lastname').fill('');
+    await expect(page.locator('div#field-error--lastname > span')).toContainText('Please fill in your last name');
+
+    // enter lastname field with special chars should show error message
+    await page.locator('#field-input--lastname').type('Test^$%£');
+    await expect(page.locator('div#field-error--lastname > span')).toContainText('This field only accepts 25 alphanumeric characters and , . ( ) / & \' - starting with alphanumeric characters');
+
+    // lastname with just a space should show error message
+    await page.keyboard.press('Backspace');
+    await page.locator('#field-input--lastname').type(' ');
+    await expect(page.locator('div#field-error--lastname > span')).toContainText('This field only accepts 25 alphanumeric characters and , . ( ) / & \' - starting with alphanumeric characters');
+
+    // lastname with a mixture of alphanumeric chars should not show error message
+    await page.locator('#field-input--lastname').fill(''); // clear the last-name field
+    await page.locator('#field-input--lastname').type('123Test');
+    // should not show error message
+    expect(await page.locator('div#field-error--lastname > span').count()).toEqual(0);
+
+    // clear the last name field
+    await page.locator('#field-input--lastname').fill('');
+
+    // entering valid input fields should be able to submit the form
+    await commands.populateFormFields();
+
+    // submit the form
+    await page.locator('button[type=submit]').click();
+
+    await expect(page.locator('div > h1')).toContainText('Thank you,\n' +
+      'test!');
+
+    await page.close();
+  });
+});
---
+++ e2e/submit/postcodeLookup.spec.js
@@ -0,0 +1,120 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test.describe('Postcode validation', () => {
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/', { timeout: 30000 });
+
+    await page.waitForLoadState('domcontentloaded');
+
+    await page.waitForSelector('#field-label--giftaid');
+    await page.locator('#field-label--giftaid').click();
+
+    // fill in all input fields
+    await page.locator('#field-input--mobile').fill('07123456789');
+    await page.locator('input#field-input--firstname').fill('test');
+    await page.locator('input#field-input--lastname').fill('user');
+  });
+
+  test('postcode entered with extra spaces should show error message', async ({ page }) => {
+
+    await page.locator('input#field-input--postcode').type('S E 1 7 T P');
+
+    await expect(page.locator('div#field-error--postcode > span')).toBeVisible();
+
+    await expect(page.locator('div#field-error--postcode > span')).toContainText('Please enter a valid UK postcode, using a space and capital letters');
+
+    await page.close();
+  });
+
+  test('postcode entered in lowercase should show error message', async ({ page }) => {
+
+    await page.locator('input#field-input--postcode').type('se17tp');
+
+    await expect(page.locator('div#field-error--postcode > span')).toBeVisible();
+
+    await expect(page.locator('div#field-error--postcode > span')).toContainText('Please enter a valid UK postcode, using a space and capital letters');
+
+    await page.close();
+  });
+
+  test('postcode entered with no spaces should show error message', async ({ page }) => {
+
+    await page.locator('input#field-input--postcode').type('SE17TP');
+
+    await expect(page.locator('div#field-error--postcode > span')).toBeVisible();
+
+    await expect(page.locator('div#field-error--postcode > span')).toContainText('Please enter a valid UK postcode, using a space and capital letters');
+
+    await page.close();
+  });
+
+  test('postcode entered with special characters should show error message', async ({ page }) => {
+
+    await page.locator('input#field-input--postcode').type('SE$%TP');
+
+    await expect(page.locator('div#field-error--postcode > span')).toBeVisible();
+
+    await expect(page.locator('div#field-error--postcode > span')).toContainText('Please enter a valid UK postcode, using a space and capital letters');
+
+    await page.close();
+  });
+
+  test('enter valid UK postcode using postcode lookup should be able to submit the form', async ({ page }) => {
+
+    // enter postcode
+    await page.locator('input#field-input--postcode').fill('SE1 7TP');
+    // click on postcode lookup button
+    await page.locator('#postcode_button').click();
+
+    if (await page.locator('#field-select--addressSelect').isVisible()) {
+      console.log('postcode lookup address dropdown present select the address');
+
+      await expect(page.locator('#field-select--addressSelect')).toBeVisible();
+
+      await page.waitForSelector('select#field-select--addressSelect');
+
+      const optionToSelect = await page.locator('option', { hasText: 'COMIC RELIEF, CAMELFORD HOUSE 87-90' }).textContent();
+      console.log('selected option: ', optionToSelect);
+
+      // Use option text to select
+      await page.locator('select#field-select--addressSelect').selectOption({ label: optionToSelect });
+
+      // expect pre-enetered se17tp postcode to change to SE1 7TP when address is selected by removing the extra spaces
+      await expect(page.locator('input#field-input--postcode')).toHaveValue('SE1 7TP');
+
+      const addressLine1 = await page.evaluate(() => document.querySelector('#field-input--address1').getAttribute('value'));
+      console.log('Address line 1 field value is : ', addressLine1);
+
+      const addressLine2 = await page.evaluate(() => document.querySelector('#field-input--address2').getAttribute('value'));
+      console.log('Address line 1 field value is : ', addressLine2);
+
+      const addressLine3 = await page.evaluate(() => document.querySelector('#field-input--address3').getAttribute('value'));
+      console.log('Address line 1 field value is : ', addressLine3);
+
+      const town = await page.evaluate(() => document.querySelector('input#field-input--town').getAttribute('value'));
+      console.log('Address line 1 field value is : ', town);
+
+      // clicking on submit button should show error on address lookup
+      await page.locator('button[type=submit]').click();
+
+      await expect(page.locator('div > h1')).toContainText('Thank you,\n' +
+        'test!');
+    } else {
+
+      // click on manual address link
+      await page.locator('a[aria-describedby=field-error--addressDetails]').click();
+      await page.locator('#field-input--address1').type('COMIC RELIEF');
+      await page.locator('#field-input--address2').type('CAMELFORD HOUSE 87-90');
+      await page.locator('#field-input--address3').type('ALBERT EMBANKMENT');
+      await page.locator('#field-input--town').type('LONDON');
+      // clicking on submit button should show error on address lookup
+      await page.locator('button[type=submit]').click();
+
+      await expect(page.locator('div > h1')).toContainText('Thank you,\n' +
+        'test!');
+    }
+    await page.close();
+  });
+});
