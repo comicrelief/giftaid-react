a4b9b2f3ab093081b7d35e7036bebb3302a3d50a
Andy E Phipps <a.phipps@comicrelief.com>
2023-08-10 09:12:11 +0100

    Update footer address (#396)

--- src/components/App/FooterCopy.js
+++ src/components/App/FooterCopy.js
@@ -2,11 +2,11 @@ const FooterCopy = {
 
     SR: 'Sport Relief is an initiative of Comic Relief. Comic Relief is the trading name of Charity Projects, ' +
     'a registered charity in England and Wales (326568) and Scotland (SC039730), which is a company limited by' +
-        'guarantee registered in England and Wales (01806414). Registered address: 89 Albert Embankment, London SE1 7TP.',
+        'guarantee registered in England and Wales (01806414). Registered address: 6th Floor, The White Chapel Building, 10 Whitechapel High Street, London, E1 8QS.',
   
     CR: 'Comic Relief is the trading name of Charity Projects, a registered charity in England and Wales ' +
     '(326568) and Scotland (SC039730), which is a company limited by guarantee registered in England and Wales ' +
-    '(01806414). Registered address: 1st Floor, 89 Albert Embankment, London, SE1 7TP.',
+    '(01806414). Registered address: 6th Floor, The White Chapel Building, 10 Whitechapel High Street, London, E1 8QS.',
   
     BIGNIGHTIN: 'Big Night In is a joint initiative between BBC Children in Need and Comic Relief. ' +
     'This website and donation page is managed by Comic Relief for The Big Night In. ' +
@@ -15,7 +15,7 @@ const FooterCopy = {
     'registered office is at Bridge House, Media City UK, Salford, England, M50 2BH. ' +
     'Comic Relief is the trading name of Charity Projects, a registered charity in England and ' +
     'Wales (326568) and Scotland (SC039730), which is a company limited by guarantee registered in England and Wales (01806414). ' +
-    'Registered address: 1st Floor, 89 Albert Embankment, London, SE1 7TP.',
+    'Registered address: 6th Floor, The White Chapel Building, 10 Whitechapel High Street, London, E1 8QS.',
   };
   
   export default FooterCopy;
