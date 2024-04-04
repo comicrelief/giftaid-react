bcd62c871dd3dbb8c0c87fba97f482c586bb632c
AndyEPhipps <a.phipps@comicrelief.com>
2024-04-04 11:41:42 +0100

    Update config to suit updated package

--- config/env.js
+++ config/env.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 const paths = require('./paths');
+const dotenv = require('dotenv');
 
 // Make sure that including paths.js after env.js will read .env variables.
 delete require.cache[require.resolve('./paths')];
@@ -32,11 +33,9 @@ var dotenvFiles = [
 // https://github.com/motdotla/dotenv-expand
 dotenvFiles.forEach(dotenvFile => {
   if (fs.existsSync(dotenvFile)) {
-    require('dotenv-expand')(
-      require('dotenv').config({
+      dotenv.config({
         path: dotenvFile,
-      })
-    );
+      });
   }
 });
 
