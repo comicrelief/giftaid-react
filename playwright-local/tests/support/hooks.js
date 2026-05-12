65a24df47fdb092baaba53daed71180b76df5e70
Krupa Pammi <k.pammi@comicrelief.com>
2026-05-12 13:49:06 +0100

    test: Add mobile safari config and parallel scripts

--- playwright-local/tests/support/hooks.js
+++ playwright-local/tests/support/hooks.js
@@ -1,30 +1,45 @@
 require('dotenv').config();
 
 const { Before, After, setDefaultTimeout } = require('@cucumber/cucumber');
-const { chromium } = require('@playwright/test');
+const { chromium, webkit, devices } = require('@playwright/test');
 const { Commands } = require('../utils/commands');
 
 setDefaultTimeout(300 * 1000);
 
 // Runs before each scenario
 Before(async function () {
-  this.browser = await chromium.launch({
-    channel: 'chrome',
-    headless: process.env.HEADED !== 'true',
-  });
+  const browserName = process.env.BROWSER || 'chromium';
+  const isMobileSafari = browserName === 'mobile-safari';
   
-  this.context = await this.browser.newContext({
-    viewport: {
-      width: 1300,
-      height: 1000,
-    },
-    serviceWorkers: 'block', // Avoids caching issues
-  });
+  this.browser = isMobileSafari
+    ? await webkit.launch({
+      headless: process.env.HEADED !== 'true',
+    })
+    : await chromium.launch({
+      channel: 'chrome',
+      headless: process.env.HEADED !== 'true',
+    });
+  
+  this.context = await this.browser.newContext(
+    isMobileSafari
+      ? {
+        ...devices['iPhone 12'],
+        serviceWorkers: 'block',
+      }
+      : {
+        viewport: {
+          width: 1300,
+          height: 1000,
+        },
+        serviceWorkers: 'block',
+      }
+  );
   
   this.page = await this.context.newPage();
   this.commands = new Commands(this.page);
 });
 
+// Runs after each scenario
 After(async function () {
   if (this.page) {
     await this.page.close();
