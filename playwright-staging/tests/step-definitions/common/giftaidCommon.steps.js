01353cca501a70feb38d3c91f2821b60bd3c676b
Krupa Pammi <k.pammi@comicrelief.com>
2026-05-19 12:52:31 +0100

    test: move resuable steps into giftaid common

--- playwright-staging/tests/step-definitions/common/giftaidCommon.steps.js
+++ playwright-staging/tests/step-definitions/common/giftaidCommon.steps.js
@@ -1,9 +1,26 @@
-const { When, Then } = require('@cucumber/cucumber');
+const { When, Then, Given} = require('@cucumber/cucumber');
 const { expect } = require('@playwright/test');
 const Chance = require('chance');
 const { selectors } = require('../../utils/locators');
 
 const chance = new Chance();
+
+Given('I am on the Giftaid page', async function () {
+  await this.page.goto(process.env.BASE_URL, { timeout: 30000 });
+  await this.page.waitForLoadState('domcontentloaded');
+});
+
+When('I select the Giftaid option', async function () {
+  await this.page.click(selectors.giftaid.option);
+});
+
+Then('I should see the Giftaid thank you message', async function () {
+  const expectedFirstName = this.supporter?.firstName || 'test';
+  await expect(this.page.locator('h1')).toContainText(
+    `Thank you, ${expectedFirstName}!`,
+    { timeout: 30000 }
+  );
+});
 
 When('I submit the Giftaid form', async function () {
   await this.page.locator(selectors.formFields.submitButton).click();
